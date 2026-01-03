b731358d34ec25f9c94d97640af553ee397bc4ee
psjw <dlrudtn108@gmail.com>
2026-01-04 00:36:44 +0900

    component basic

---
+++ src/components/TheView.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<main>
+			<div class="container py-4">
+				<div class="container">
+					<div class="row g-3">
+						<div class="col-4">
+							<AppCard></AppCard>
+						</div>
+						<div class="col-4"><AppCard></AppCard></div>
+						<div class="col-4"><AppCard></AppCard></div>
+						<div class="col-4"><AppCard></AppCard></div>
+						<div class="col-4"><AppCard></AppCard></div>
+						<div class="col-4"><AppCard></AppCard></div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+import AppCard from '@/components/AppCard.vue';
+export default {
+	components: {
+		AppCard,
+	},
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
