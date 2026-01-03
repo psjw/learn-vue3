1c4adefd92be17b15b2acd7020d8aeed1e598b26
psjw <dlrudtn108@gmail.com>
2026-01-03 12:38:48 +0900

    component API, Setup, Template Syntax

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  },
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier',
+	],
+	env: {
+		'vue/setup-compiler-macros': true,
+	},
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -29,4 +29,4 @@ module.exports = {
 			},
 		],
 	},
-}
+};
---
+++ src/components/lecture/6/App_TemplateSyntax.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<h2>보간법</h2>
+		<p>{{ messages }}</p>
+		<p v-once>{{ messages }}</p>
+		<button v-on:click="messages = messages + '!'">Click!</button>
+		<hr />
+		<h2>HTML</h2>
+		<p>{{ rawHtml }}</p>
+		<p v-html="rawHtml"></p>
+		<hr />
+		<h2>속성바인딩</h2>
+		<div :title="dynamicTitle">마우스를 올려보세요</div>
+		<input type="text" value="홍길동" :disabled="isInputDisabled" />
+		<br />
+		<input v-bind="attrs" />
+		<h2>JavaScript</h2>
+		{{ messages.split('').reverse().join('') }}
+		<br />
+		{{ isInputDisabled ? '예' : '아니오' }}
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const messages = ref('안녕하세요');
+		const rawHtml = ref('<strong>안녕하세요</strong>');
+
+		const dynamicTitle = ref('안녕하세요!!!@@@');
+		const isInputDisabled = ref(true);
+		const attrs = {
+			type: 'text',
+			value: '12345678',
+			disabled: false,
+		};
+		return {
+			messages,
+			rawHtml,
+			dynamicTitle,
+			isInputDisabled,
+			attrs,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
