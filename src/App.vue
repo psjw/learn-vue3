303185e7c153b0be8526029e854718568090a8fc
psjw <dlrudtn108@gmail.com>
2026-01-04 00:19:52 +0900

    bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,152 @@
 <template>
 	<div>
-		<form @submit.prevent>
-			<input v-model.lazy="title" type="text" placeholder="Title" />
-			<textarea v-model.lazy="contents" placeholder="Contents"></textarea>
-			<hr />
-			<button @click="save(title, contents)">저장</button>
-		</form>
+		<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a
+								class="nav-link dropdown-toggle"
+								href="#"
+								id="navbarDropdown"
+								role="button"
+								data-bs-toggle="dropdown"
+								aria-expanded="false"
+							>
+								Dropdown
+							</a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<li><a class="dropdown-item" href="#">Action</a></li>
+								<li><a class="dropdown-item" href="#">Another action</a></li>
+								<li><hr class="dropdown-divider" /></li>
+								<li>
+									<a class="dropdown-item" href="#">Something else here</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled">Disabled</a>
+						</li>
+					</ul>
+					<form class="d-flex">
+						<input
+							class="form-control me-2"
+							type="search"
+							placeholder="Search"
+							aria-label="Search"
+						/>
+						<button class="btn btn-outline-success" type="submit">
+							Search
+						</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+		<main>
+			<div class="container py-4">
+				<div class="container">
+					<div class="row g-3">
+						<div class="col-4">
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">
+										Some quick example text to build on the card title and make
+										up the bulk of the card's content.
+									</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-4">
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">
+										Some quick example text to build on the card title and make
+										up the bulk of the card's content.
+									</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-4">
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">
+										Some quick example text to build on the card title and make
+										up the bulk of the card's content.
+									</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-4">
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">
+										Some quick example text to build on the card title and make
+										up the bulk of the card's content.
+									</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-4">
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">
+										Some quick example text to build on the card title and make
+										up the bulk of the card's content.
+									</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-4">
+							<div class="card">
+								<div class="card-body">
+									<h5 class="card-title">Card title</h5>
+									<p class="card-text">
+										Some quick example text to build on the card title and make
+										up the bulk of the card's content.
+									</p>
+									<a href="#" class="btn btn-primary">Go somewhere</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
 	</div>
 </template>
 
 <script>
-import { ref, watchEffect } from 'vue';
-
 export default {
 	setup() {
-		const title = ref('');
-		const contents = ref('');
-
-		const save = (title, contents) => {
-			console.log('저장되었습니다. title: ${title}, contents: ${contents}');
-		};
-		watchEffect(() => {
-			console.log('watchEffect');
-			save(title.value, contents.value);
-		});
-		return {
-			title,
-			contents,
-			save,
-		};
+		return {};
 	},
 };
 </script>
