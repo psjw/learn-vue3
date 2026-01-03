f9b61ef2fd6ce32bb1508a5f370c2d4c0b436c00
psjw <dlrudtn108@gmail.com>
2026-01-03 17:58:34 +0900

    v-if

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <template>
-	<div>
-		<div :style="styleObject">
-			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam quasi sint
-			veritatis ex doloremque esse nostrum asperiores quisquam vitae cupiditate,
-			qui maiores magnam aliquam voluptatem impedit facilis sit architecto
-			nesciunt!
-		</div>
-		<button v-on:click="fontSize--">-</button>
-		<button v-on:click="fontSize++">+</button>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { computed, reactive, ref } from 'vue';
 export default {
 	setup() {
-		// const styleObject = reactive({
-		// 	color: 'red',
-		// 	fontSize: '13px',
-		// });
-		const fontSize = ref(13);
-		const styleObject = computed(() => {
-			return {
-				color: 'red',
-				fontSize: fontSize.value + 'px',
-			};
-		});
-		return {
-			styleObject,
-			fontSize,
-		};
+		return {};
 	},
 };
 </script>
